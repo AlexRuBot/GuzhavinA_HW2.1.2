0d53bc6ac47d26cbb5c73df676eb97749665fb3a
Саша Гужавин <sashaguzavin@gmail.com>
2019-09-19 01:31:31 +0300

    Initial Commit

---
+++ GuzhavinA_HW2.1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuzhavinA_HW2.1.2
+//
+//  Created by Саша Гужавин on 19/09/2019.
+//  Copyright © 2019 Sasha Guzhavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
