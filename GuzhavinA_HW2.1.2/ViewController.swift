5a9bbb8da6a2f514504b5ccbb6f31289dae44ff4
Саша Гужавин <sashaguzavin@gmail.com>
2019-09-19 02:44:35 +0300

    Test

--- GuzhavinA_HW2.1.2/ViewController.swift
+++ GuzhavinA_HW2.1.2/ViewController.swift
@@ -9,8 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redCircle: UIView!
+    @IBOutlet weak var yellowCircle: UIView!
+    @IBOutlet weak var greenCircle: UIView!
+    @IBOutlet weak var outletButton: UIButton!
+    @IBAction func actionButton(_ sender: UIButton) {
+        
+        if redCircle.alpha < 1, yellowCircle.alpha < 1, greenCircle.alpha < 1 {
+            outletButton.setTitle("NEXT", for: .normal)
+            redCircle.alpha = 1
+        } else if redCircle.alpha == 1, yellowCircle.alpha < 1, greenCircle.alpha < 1 {
+            redCircle.alpha = 0.3
+            yellowCircle.alpha = 1
+        } else if yellowCircle.alpha == 1, greenCircle.alpha < 1, redCircle.alpha < 1 {
+            yellowCircle.alpha = 0.3
+            greenCircle.alpha = 1
+        } else if greenCircle.alpha == 1, redCircle.alpha < 1, yellowCircle.alpha < 1 {
+            greenCircle.alpha = 0.3
+            redCircle.alpha = 1
+        }
+        
+    }
+    
     override func viewDidLoad() {
+        
+        outletButton.backgroundColor = .blue
+        redCircle.alpha = 0.3
+        redCircle.layer.cornerRadius = 50
+        yellowCircle.alpha = 0.3
+        yellowCircle.layer.cornerRadius = 50
+        greenCircle.alpha = 0.3
+        greenCircle.layer.cornerRadius = 50
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
